be5ada00b5e5ecf3426750262356288f44368ccf
galaxyjim <galaxyjim@yahoo.com>
2015-04-01 20:08:56 -0400

    add text property to edit box

--- samples_and_tests/EditBox.py
+++ samples_and_tests/EditBox.py
@@ -104,17 +104,33 @@ class EditBox(Widget):
         self.caret.visible = True
         self.caret.mark = 0
         self.caret.position = len(self._elements['layout'].document.text)
+
+    #
+    def _get_text(self):
+        return self.document.text
+
+    def _set_text(self, text):
+        self.document.text = text
+
+    text = property(_get_text, _set_text,
+        doc='''text data displayed by the widget
+
+        :type: string
+        ''')
+
  
 # pyglet window
 window = pyglet.window.Window(resizable=True)
 
 # pywidget
-ebox1 = EditBox(text='Text Here. abcdefghijklmnopqrstuvwxyzWTLM',
-                x=50, y=75, window=window)
-ebox2 = EditBox(text='second line',
-                x=50, y=175, window=window)
-button = Button(text="BBBB", x=50, y=125)
-desktop = Canvas(x=0, y=0, width=640, height=480, elements=[ebox1, button, ebox2])
+label1 = Label( text="<font color=white>1</font>", x=0, y=75, width=50)
+ebox1 = EditBox(text='bottom line',
+                x=50, y=75, width=200, window=window)
+label2 = Label( text="<font color=white>2</font>", x=0, y=175, width=50)
+ebox2 = EditBox(text='top line',
+                x=50, y=175, width=200, window=window)
+button = Button(text="<font color=white>Copy 1 to 2</font>", x=50, y=125)
+desktop = Canvas(x=0, y=0, width=640, height=480, elements=[label1, ebox1, label2, button, ebox2])
 
 # pyglet event hander for pywidget
 window.push_handlers(desktop)
@@ -125,6 +141,10 @@ def on_draw():
     window.clear()
     desktop.on_draw()
 
-#
+@button.event
+def on_button_press(button):
+    print(ebox1.text)
+    ebox2.text = ebox1.text
+    
 pyglet.app.run()
 
